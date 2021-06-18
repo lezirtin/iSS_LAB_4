91ab79744b9cb803562f21c75ff6c16e762cde13
lezirtin <lokecoder@gmail.com>
2021-06-18 17:22:50 +0530

    final

---
+++ script.js
@@ -0,0 +1,18 @@
+function fun1() {
+    document.getElementById('firstImage').src = "images/Covishield.jpg"
+}
+function fun2() {
+    document.getElementById('firstImage').src = "images/Covax.jpg"
+}
+function fun3() {
+    document.getElementById('firstImage').src = "images/Sputnik.jpg"
+}
+function fun11() {
+    document.getElementsByTagName("body")[0].style.background = "lightgreen"
+}
+function fun22() {
+    document.getElementsByTagName("body")[0].style.background = "palevioletred"
+}
+function fun33() {
+    document.getElementsByTagName("body")[0].style.background = "lightblue"
+}
